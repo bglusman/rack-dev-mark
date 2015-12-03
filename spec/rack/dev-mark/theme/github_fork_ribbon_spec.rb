b227062f25791e0a4adbdae7e3ada0a2d0089eb3
Brian Glusman <bglusman@stridenyc.com>
2015-12-03 16:18:45 -0500

    v1 working, do not target body in quotes, prefer to make not in tag

--- spec/rack/dev-mark/theme/github_fork_ribbon_spec.rb
+++ spec/rack/dev-mark/theme/github_fork_ribbon_spec.rb
@@ -64,4 +64,15 @@ describe Rack::DevMark::Theme::GithubForkRibbon do
       subject { Rack::DevMark::Theme::GithubForkRibbon.new(fixed: true) }
     end
   end
+  context "finds body not in script tags" do
+    let :input do
+      <<-EOS
+<html><head>head<title>title</title></head><script>o.write('<body onload="document._l();">');</script><style>
+      EOS
+    end
+    it "ignores a body inside scrpt tag" do
+      subject { Rack::DevMark::Theme::GithubForkRibbon.new(fixed: true) }
+      expect(subject.insert_into(input, 'env', revision: 'rev', timestamp: 'time')).to eq(input)
+    end
+  end
 end
